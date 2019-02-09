fa5468edf4682cfa7ec1cf2568c3dfad00da554a
liangganglovelife <894195022@qq.com>
2019-02-09 17:58:33 +0800

    first commit

---
+++ build/precache-manifest.b84b12e67dd055440e74838256ff84ff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a702708667ae2b07bcd6",
+    "url": "/static/js/main.a7027086.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "2fa79094baf4143f32dd4584dfeb7cc6",
+    "url": "/index.html"
+  }
+];
